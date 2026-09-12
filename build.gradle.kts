2b45583666933032f0a11bfc7e9031fecb4e38a7
VastCoreOS3 <zhangzhe250496549@qq.com>
2026-09-12 20:07:11 +0800

    Add files via upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,11 @@
+import com.android.build.api.dsl.ApplicationDefaultConfig
+import com.android.build.api.dsl.CommonExtension
+import com.android.build.gradle.api.AndroidBasePlugin
+
 plugins {
     alias(libs.plugins.agp.app) apply false
+    alias(libs.plugins.agp.lib) apply false
+    alias(libs.plugins.kotlin) apply false
     alias(libs.plugins.kotlin.compose.compiler) apply false
 }
 
@@ -10,9 +16,10 @@ val androidTargetSdkVersion by extra(37)
 val androidCompileSdkVersion by extra(37)
 val androidBuildToolsVersion by extra("36.1.0")
 val androidCompileNdkVersion by extra("29.0.14206865")
+
 val managerVersionCode by extra(getVersionCode())
 val managerVersionName by extra(getVersionName())
-val branchName by extra(getBranch())
+
 fun Project.exec(command: String) = providers.exec {
     commandLine(command.split(" "))
 }.standardOutput.asText.get().trim()
@@ -31,10 +38,6 @@ fun getVersionCode(): Int {
     return major * 10000 + commitCount + 200
 }
 
-fun getBranch(): String {
-    return exec("git rev-parse --abbrev-ref HEAD").trim()
-}
-
 fun getVersionName(): String {
     return getGitDescribe()
 }
@@ -43,5 +46,29 @@ tasks.register("printVersion") {
     doLast {
         println("Version code: $managerVersionCode")
         println("Version name: $managerVersionName")
+    }
+}
+
+subprojects {
+    plugins.withType(AndroidBasePlugin::class.java) {
+        extensions.configure(CommonExtension::class.java) {
+            compileSdk = androidCompileSdkVersion
+            buildToolsVersion = androidBuildToolsVersion
+            ndkVersion = androidCompileNdkVersion
+
+            defaultConfig {
+                minSdk = androidMinSdkVersion
+                if (this is ApplicationDefaultConfig) {
+                    targetSdk = androidTargetSdkVersion
+                    versionCode = managerVersionCode
+                    versionName = managerVersionName
+                }
+            }
+
+            lint {
+                abortOnError = true
+                checkReleaseBuilds = false
+            }
+        }
     }
 }
