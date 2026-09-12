38104443790f7e2dbfeb2a86283a18046e9dd335
VastCoreOS3 <zhangzhe250496549@qq.com>
2026-09-12 20:12:10 +0800

    Add files via upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,6 @@ import com.android.build.gradle.api.AndroidBasePlugin
 
 plugins {
     alias(libs.plugins.agp.app) apply false
-    alias(libs.plugins.agp.lib) apply false
-    alias(libs.plugins.kotlin) apply false
     alias(libs.plugins.kotlin.compose.compiler) apply false
 }
 
