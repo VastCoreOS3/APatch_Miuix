cf95a1fc1685fc5b6945ca34398ca1343c78efe2
VastCoreOS3 <zhangzhe250496549@qq.com>
2026-09-14 12:20:11 +0800

    Add files via upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,9 +26,7 @@ fun getGitDescribe(): String {
 }
 
 fun getVersionCode(): Int {
-    val commitCount = getGitCommitCount()
-    val major = 1
-    return major * 10000 + commitCount + 200
+    return 12238
 }
 
 fun getBranch(): String {
@@ -36,7 +34,7 @@ fun getBranch(): String {
 }
 
 fun getVersionName(): String {
-    return getVersionCode().toString()
+    return "12238"
 }
 
 tasks.register("printVersion") {
