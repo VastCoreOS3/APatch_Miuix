98d0db780523044396484f6b41640d74c083285d
VastCoreOS3 <zhangzhe250496549@qq.com>
2026-09-15 16:40:28 +0800

    Add files via upload

--- app/src/main/java/me/bmax/apatch/ui/component/BottomBar.kt
+++ app/src/main/java/me/bmax/apatch/ui/component/BottomBar.kt
@@ -29,6 +29,7 @@ import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.alpha
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
