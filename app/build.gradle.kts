498fa07eabdd13ce27ee22acb807ad199016df21
VastCoreOS3 <zhangzhe250496549@qq.com>
2026-09-15 22:11:03 +0800

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -229,6 +229,13 @@ fun downloadFile(url: String, destFile: File) {
 }
 
 registerDownloadTask(
+    taskName = "downloadKpimg",
+    srcUrl = "https://github.com/VastCoreOS3/KernelPatch/releases/download/$kernelPatchVersion/kpimg-android",
+    destPath = "${project.projectDir}/src/main/assets/kpimg",
+    project = project
+)
+
+registerDownloadTask(
     taskName = "downloadKptools",
     srcUrl = "https://github.com/bmax121/KernelPatch/releases/download/$kernelPatchVersion/kptools-android",
     destPath = "${project.projectDir}/libs/arm64-v8a/libkptools.so",
@@ -255,6 +262,7 @@ tasks.register<Copy>("mergeScripts") {
 }
 
 tasks.getByName("preBuild").dependsOn(
+    "downloadKpimg",
     "downloadKptools",
     "downloadCompatKpatch",
     "mergeScripts",
