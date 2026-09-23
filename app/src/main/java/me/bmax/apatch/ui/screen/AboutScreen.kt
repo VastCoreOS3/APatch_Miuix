fd5e58353073e4db77fb319df0cb8184086a938b
VastCoreOS3 <zhangzhe250496549@qq.com>
2026-09-23 08:38:50 +0800

    Add files via upload

--- app/src/main/java/me/bmax/apatch/ui/screen/AboutScreen.kt
+++ app/src/main/java/me/bmax/apatch/ui/screen/AboutScreen.kt
@@ -79,10 +79,11 @@ private val LightGradientPalettes = listOf(
     listOf(Color(0.98f, 0.86f, 0.90f), Color(0.60f, 0.73f, 0.98f), Color(0.92f, 0.93f, 1f), Color(0.56f, 0.69f, 1f)),
 )
 private val DarkGradientPalettes = listOf(
-    listOf(Color(0.20f, 0.06f, 0.88f, 0.40f), Color(0.30f, 0.14f, 0.55f, 0.50f), Color(0f, 0.64f, 0.96f, 0.50f), Color(0.11f, 0.16f, 0.83f, 0.40f)),
-    listOf(Color(0.07f, 0.15f, 0.79f, 0.50f), Color(0.62f, 0.21f, 0.67f, 0.50f), Color(0.06f, 0.25f, 0.84f, 0.50f), Color(0f, 0.20f, 0.78f, 0.50f)),
-    listOf(Color(0.58f, 0.30f, 0.74f, 0.40f), Color(0.27f, 0.18f, 0.60f, 0.50f), Color(0.66f, 0.26f, 0.62f, 0.50f), Color(0.12f, 0.16f, 0.70f, 0.60f)),
+    listOf(Color(0.02f, 0.10f, 0.35f, 0.32f), Color(0.05f, 0.22f, 0.48f, 0.36f), Color(0.18f, 0.08f, 0.42f, 0.30f), Color(0.04f, 0.14f, 0.40f, 0.34f)),
+    listOf(Color(0.03f, 0.16f, 0.44f, 0.33f), Color(0.22f, 0.07f, 0.38f, 0.31f), Color(0.01f, 0.18f, 0.42f, 0.35f), Color(0.02f, 0.11f, 0.36f, 0.30f)),
+    listOf(Color(0.20f, 0.09f, 0.36f, 0.30f), Color(0.06f, 0.12f, 0.38f, 0.34f), Color(0.24f, 0.10f, 0.34f, 0.32f), Color(0.03f, 0.09f, 0.32f, 0.36f)),
 )
+
 
 /**
  * color_mode: 0=自动跟随系统，1=浅色，2=深色
